05f4e5317ff0fafe3472c28233ef0ad39b69d548
Nocktowl <mehedi.hasanhulknabil@gmail.com>
2026-08-30 11:22:36 +0600

    feat: update server configuration with proxy and public URL settings

--- server/config/server.ts
+++ server/config/server.ts
@@ -3,6 +3,8 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
+  url: env('PUBLIC_URL', "https://cps-academy-lms.onrender.com"),
   app: {
     keys: env.array('APP_KEYS')!,
   },
