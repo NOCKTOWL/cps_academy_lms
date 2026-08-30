a0e22e395701d91bf095af38e2f9008b3f51e481
Nocktowl <mehedi.hasanhulknabil@gmail.com>
2026-08-30 11:26:37 +0600

    feat: update server configuration to enable Koa proxy support

--- server/config/server.ts
+++ server/config/server.ts
@@ -3,7 +3,9 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: true,
+  proxy: {
+    koa: true,
+  },
   url: env('PUBLIC_URL', "https://cps-academy-lms.onrender.com"),
   app: {
     keys: env.array('APP_KEYS')!,
